8ae72463c239889c9d340c909348da6669713251
samuelpd22 <samuel050101@hotmail.com>
2024-09-19 20:50:31 -0300

    feat : add metodos de autor

---
+++ src/main/java/br/com/livrariasl/repository/AutorRepository.java
@@ -0,0 +1,9 @@
+
+package br.com.livrariasl.repository;
+
+
+import br.com.livrariasl.entity.AutorEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AutorRepository extends JpaRepository<AutorEntity, Long> {
+}
---
+++ src/main/java/br/com/livrariasl/service/LivrosService.java
@@ -0,0 +1,40 @@
+package br.com.livrariasl.service;
+
+import br.com.livrariasl.dto.LivrosDTO;
+import br.com.livrariasl.entity.LivrosEntity;
+import br.com.livrariasl.repository.LivrosRepository;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class LivrosService {
+
+    @Autowired
+    private LivrosRepository livrosRepository;
+
+
+    public List<LivrosEntity> getAll(){
+        return livrosRepository.findAll();
+    }
+
+    public Optional<LivrosEntity> getById(Long id){
+        return livrosRepository.findById(id);
+    }
+
+
+    public LivrosEntity registraLivro(LivrosDTO livrosDTO){
+        LivrosEntity newLivro = new LivrosEntity();
+        BeanUtils.copyProperties(livrosDTO , newLivro);
+        return livrosRepository.save(newLivro);
+    }
+
+    public String apagarLivro(Long id){
+        livrosRepository.deleteById(id);
+        return "Livro apagado com sucesso";
+    }
+
+}
