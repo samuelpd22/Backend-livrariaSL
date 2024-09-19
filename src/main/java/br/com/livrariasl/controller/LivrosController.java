8a745ed4c54dc15f3fbea5bef6ced7afb0b8ec0c
samuelpd22 <samuel050101@hotmail.com>
2024-09-19 20:49:02 -0300

    feat : divindo responsabilidades entre as classes

--- src/main/java/br/com/livrariasl/controller/LivrosController.java
+++ src/main/java/br/com/livrariasl/controller/LivrosController.java
@@ -4,6 +4,7 @@ import br.com.livrariasl.dto.LivrosDTO;
 import br.com.livrariasl.entity.Genero;
 import br.com.livrariasl.entity.LivrosEntity;
 import br.com.livrariasl.repository.LivrosRepository;
+import br.com.livrariasl.service.LivrosService;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -19,30 +20,37 @@ public class LivrosController {
     @Autowired
     private LivrosRepository livrosRepository;
 
+    @Autowired
+    private LivrosService livrosService;
+
+
+
+
     @PostMapping("/enviar")
     public ResponseEntity<LivrosEntity> enviarLivro(@RequestBody LivrosDTO livrosDTO){
-        LivrosEntity newLivro = new LivrosEntity();
-        BeanUtils.copyProperties(livrosDTO, newLivro);
-        livrosRepository.save(newLivro);
-
-        return ResponseEntity.ok(newLivro);
+        return ResponseEntity.ok(livrosService.registraLivro(livrosDTO));
 
     }
     @GetMapping
     public ResponseEntity<List> verLivros(){
-        return new ResponseEntity(livrosRepository.findAll(), HttpStatus.OK);
+        return new ResponseEntity(livrosService.getAll(), HttpStatus.OK);
     }
+
+
 
     @GetMapping("/pegar/{id}")
     public ResponseEntity<LivrosEntity> pegarPorId(@PathVariable Long id) {
-        return new ResponseEntity(livrosRepository.findById(id),HttpStatus.OK);
+        return new ResponseEntity(livrosService.getById(id),HttpStatus.OK);
     }
 
 
+
+
+
+    // Sem utilizade até o momento
     @DeleteMapping("/delete/{id}")
     public ResponseEntity<?> deletarPorId(@PathVariable Long id) {
-        livrosRepository.deleteById(id);
-        return new ResponseEntity(HttpStatus.OK);
+        return ResponseEntity.ok(livrosService.apagarLivro(id));
     }
 
 
