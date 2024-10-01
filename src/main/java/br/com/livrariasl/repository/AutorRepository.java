9e979d051f1680bdded1841ba29210124c5503aa
samuelpd22 <samuel050101@hotmail.com>
2024-10-01 19:52:48 -0300

    feat : metodo pra buscar autor

--- src/main/java/br/com/livrariasl/repository/AutorRepository.java
+++ src/main/java/br/com/livrariasl/repository/AutorRepository.java
@@ -3,7 +3,15 @@ package br.com.livrariasl.repository;
 
 
 import br.com.livrariasl.entity.AutorEntity;
+import br.com.livrariasl.entity.Genero;
+import br.com.livrariasl.entity.LivrosEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface AutorRepository extends JpaRepository<AutorEntity, Long> {
+
+
+    List<AutorEntity> findByNome(String nome);
+
 }
